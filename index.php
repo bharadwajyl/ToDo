cf6c859174b9ef2e23d2889f8cc15afb522255b0
Mahesh <100456710+bharadwajyl@users.noreply.github.com>
2022-10-22 11:27:24 +0000

    Update index.php

--- index.php
+++ index.php
@@ -97,25 +97,3 @@ $list = $conn->query($sql);
 <script type="text/javascript" src="assets/js/main.js"></script>
 </body>
 </html>
-
-
-<?php
-//Palindrome
-$string = "babad";
-$array = array();
-for($i=0; $i<strlen($string); $i++ )   {  
-  $palindrome = true; 
-  $n = 1;     
-  while($palindrome)  
-  { 
-    $word = substr($string, $i-$n, ($n*2)+1 ); 
-    if( $word == strrev($word) ) { 
-      $array[$word] = strlen($word);           
-    } else { 
-      $palindrome = false; 
-    } 
-    $n++; 
-  } 
-}
-print"Given string is babad <br>Largest Palindrome: ".(array_search (max($array), $array));
-?>
